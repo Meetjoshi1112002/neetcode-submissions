2f76f4646488519a9a4afc4caa9dbefc7cf53bc4
Meet Joshi <99858699+Meetjoshi1112002@users.noreply.github.com>
2026-05-27 10:39:31 +0530

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        while(i<s.Length && j < t.Length){
+            if(s[i] == t[j]){
+                Console.WriteLine(s[i]);
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+
+        return (i == s.Length);
+    }
+}
